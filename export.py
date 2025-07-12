c304c09f4d09f194c6775c421a969cc465de5a95
Maria <mdem0009@student.monash.edu>
2025-07-12 21:29:57 +1000

    updated wav export settings

--- export.py
+++ export.py
@@ -33,7 +33,7 @@ file_name = "Song.wav"
 def create_wav(data, sample_rate):
     with wave.open(file_name, 'wb') as wav_file:
         wav_file.setnchannels(1) # Single channel audio
-        wav_file.setsampwidth(1) # One byte per sample
+        wav_file.setsampwidth(2) # 2 bytes per sample
         wav_file.setframerate(sample_rate)
         wav_file.writeframes(data.tobytes())
     print(f"Wav file {file_name} created.")
